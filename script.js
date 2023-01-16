aa6f74fc6271b113627021ef14cd3192f9a2c76f
paulbasaur <pauldackeng@gmail.com>
2023-01-16 18:05:56 -0500

    add button ids and classes

--- script.js
+++ script.js
@@ -2,11 +2,17 @@ const display = document.querySelector("#display-numbers");
 display.textContent = "0";
 
 let displayUpdate = "";
-
-const buttons = document.querySelectorAll(".standard-button");
-buttons.forEach((button) => {
+const numberButtons = document.querySelectorAll(".number");
+numberButtons.forEach((button) => {
   button.addEventListener('click', () => {
     displayUpdate += button.textContent;
     display.textContent = displayUpdate;
+  });
+});
+
+const operatorButtons = document.querySelectorAll(".operator");
+operatorButtons.forEach((button) => {
+  button.addEventListener('click', () => {
+    alert(button.textContent);
   });
 });
