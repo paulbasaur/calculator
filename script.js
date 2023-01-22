5a4774a66220e836adbb43f96180c7513f61114c
paulbasaur <pauldackeng@gmail.com>
2023-01-22 12:50:47 -0500

    add operator and display functions

--- script.js
+++ script.js
@@ -1,6 +1,47 @@
-const display = document.querySelector("#display-numbers");
-display.textContent = "0";
+function add(num1, num2) {
+  return(num1 + num2);
+}
 
+function subtract(num1, num2) {
+  return(num1 - num2);
+}
+
+function multiply(num1, num2) {
+  return(num1 * num2);
+}
+
+function divide(num1, num2) {
+  return(num1 / num2);
+}
+
+function percent(num1) {
+  return(num1 * 100);
+}
+
+function negate(num1) {
+  return(num1 * -1);
+}
+
+function equals(expressionStr) {
+  //TODO
+}
+
+function updateDisplay() {
+  //TODO
+}
+
+function getExpression() {
+  // TODO
+}
+
+function clearDisplay() {
+  const display = document.querySelector("#display-numbers");
+  display.textContent = "0";
+}
+
+clearDisplay();
+
+const display = document.querySelector("#display-numbers");
 let displayUpdate = "";
 const numberButtons = document.querySelectorAll(".number");
 numberButtons.forEach((button) => {
@@ -10,9 +51,5 @@ numberButtons.forEach((button) => {
   });
 });
 
-const operatorButtons = document.querySelectorAll(".operator");
-operatorButtons.forEach((button) => {
-  button.addEventListener('click', () => {
-    alert(button.textContent);
-  });
-});
+const clearButton = document.querySelector("#clear-button");
+clearButton.addEventListener('click', clearDisplay);
