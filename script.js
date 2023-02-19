49ac7e0cb162c9c3681d34bc4c66844401ebd9ec
paulbasaur <pauldackeng@gmail.com>
2023-02-19 11:07:56 -0500

    add decimal validation and leading zero function

--- script.js
+++ script.js
@@ -22,6 +22,37 @@ function negate(num1) {
   return(num1 * -1);
 }
 
+function decimalCheck(input) {
+  const operators = ["+", "-", "*", "/"];
+  for (let operator of operators) {
+    if (input.includes(operator)){
+      const opIndex = input.indexOf(operator);
+      const slicedInput = input.slice(opIndex+1);
+      if (slicedInput.includes(".")) {return(false);}
+      return(true);
+    }
+  }
+  if (input.includes(".")) {return(false);}
+  return(true);
+}
+
+function leadingZero(input){
+  const operators = ["+", "-", "*", "/"];
+  for (let operator of operators) {
+    if (input == "" || input == "0" || input.slice(-1) == operator) {
+      return "0."
+    } else {
+      return(".");
+    }
+  }
+  return(input);
+}
+
+function parseTrailingZeros(input){
+  // TODO:
+  // parse trailing zeros after decimal operator button is pressed
+}
+
 function clearDisplay() {
   const display = document.querySelector("#display-numbers");
   display.textContent = "0";
@@ -29,27 +60,29 @@ function clearDisplay() {
 
 function updateDisplay(userInput) {
   const display = document.querySelector("#display-numbers");
-  // const lastChar = Number(userInput.slice(-1));
-  // if (isNaN(lastChar)) {
-  //   console.log("last char is NaN!");
-  // }
   display.textContent = userInput;
 }
 
 function isValidInput(userInput, button) {
   buttonClass = button.className;
+  let validInput = false;
+
   if (buttonClass.includes("number")) {
-    console.log("you clicked a number!");
+    validInput = true;
   } else if (buttonClass.includes("operator")) {
-    console.log("you clicked an operator!");
+    validInput = true;
   } else if (buttonClass.includes("decimal")) {
-    console.log("you clicked decimal!");
+    validInput = decimalCheck(userInput);
+    if (validInput) {return(userInput+leadingZero(userInput))}
   } else if (buttonClass.includes("equals")) {
-    console.log("you clicked equals!");
+    validInput = false;
   }
-  // if valid
-  return button.textContent;
-  // if invalid return original userInput
+
+  if (validInput) {
+    return userInput + button.textContent;
+  } else {
+    return userInput;
+  }
 }
 
 clearDisplay();
@@ -65,25 +98,25 @@ let userInput = "";
 
 numbers.forEach((button) => {
   button.addEventListener('click', () => {
-    userInput += isValidInput(userInput, button);
+    userInput = isValidInput(userInput, button);
     updateDisplay(userInput);
   });
 });
 
 operators.forEach((button) => {
   button.addEventListener('click', () => {
-    userInput += isValidInput(userInput, button);
+    userInput = isValidInput(userInput, button);
     updateDisplay(userInput);
   });
 });
 
 decimal.addEventListener('click', () => {
-  userInput += isValidInput(userInput, decimal);
+  userInput = isValidInput(userInput, decimal);
   updateDisplay(userInput);
 });
 
 equals.addEventListener('click', () => {
-  userInput += isValidInput(userInput, equals);
+  userInput = isValidInput(userInput, equals);
   updateDisplay(userInput);
 });
 
--- worksheet.js
+++ worksheet.js
@@ -1,18 +1,49 @@
-let num1 = Number(prompt("Enter first number"));
+const input1 = "344.";
+const input2 = "321.5";
+const input3 = "2.45-2.";
+const input4 = "98.2*23.9";
+const input5 = "344";
+const input6 = "478.5+31"
 
-let operation = prompt ("Enter operation");
+const input7 = "0";
+const input8 = "34.2+";
+const input9 = "12+3"
+const input10 = "2+";
+const input11 = "";
 
-let num2 = Number(prompt("Enter second number"));
+const inputs = [input1, input2, input3, input4, input5, input6];
 
-let result = 0;
-if (operation == "+") {
-  result = num1 + num2;
-} else if (operation == "-") {
-  result = num1 - num2;
-} else if (operation == "*") {
-  result = num1 * num2;
-} else if (operation == "/") {
-  result = num1 / num2;
+function decimalCheck(input) {
+  const operators = ["+", "-", "*", "/"];
+  for (let operator of operators) {
+    if (input.includes(operator)){
+      const opIndex = input.indexOf(operator);
+      const slicedInput = input.slice(opIndex+1);
+      if (slicedInput.includes(".")) {return(false);}
+      return(true);
+    }
+  }
+  if (input.includes(".")) {return(false);}
+  return(true);
 }
 
-console.log(result);
+function leadingZero(input){
+  const operators = ["+", "-", "*", "/"];
+  for (let operator of operators) {
+    if (input == "" || input == "0" ) {
+      return "0."
+    } else if (input.slice(-1) == operator) {
+      return(input + "0.");
+    }
+  }
+  return(input + ".");
+}
+
+console.log(leadingZero(input7));
+console.log(leadingZero(input10));
+console.log(leadingZero(input11));
+
+
+// for (let input of inputs) {
+//   console.log(decimalCheck(input));
+// }
